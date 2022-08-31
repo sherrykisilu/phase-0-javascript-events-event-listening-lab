2960affc2a3be48412798efda27d80250a9b1f0d
sherry <sherry.kisilu@studebnt.moringaschool.com>
2022-08-31 11:34:06 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+let input;
+function addingEventListener(input) {
+    input= document.getElementById('input');
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    input.addEventListener('click', clickAlert);
 }
+
